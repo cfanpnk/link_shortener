8da36e0807312549cb3d1a2a1046d63396e8481d
Nankai Pan <cfanpnk@gmail.com>
2018-09-24 09:21:36 -0500

    Remove the /s part from the URL redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'links#new'
   resources :links, only: [:create, :show, :index], param: :slug
-  get '/s/:hash_key' => 'links#go'
+  get '/:hash_key' => 'links#go'
 
   namespace :admin do
     resources :links, only: [:show, :update]
