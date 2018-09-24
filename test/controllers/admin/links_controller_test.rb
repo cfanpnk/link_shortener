f7ef064d7b8af33f350fa953ca25b58322f4fa42
Nankai Pan <cfanpnk@gmail.com>
2018-09-23 20:48:49 -0500

    Fix a bug for link expiration

--- test/controllers/admin/links_controller_test.rb
+++ test/controllers/admin/links_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class Admin::LinksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @active_link = links(:active_link)
+    @link = {
+      expired: true
+    }
+  end
+  test "should expire a shortened URL" do
+    patch admin_link_url(@active_link), params: { link: @link }
+    assert_redirected_to root_path
+  end
 end
