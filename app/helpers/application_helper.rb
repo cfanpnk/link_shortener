1e68fff8947033b73821fbb945cce8835e92354f
Nankai Pan <cfanpnk@gmail.com>
2018-09-24 23:01:45 -0500

    Remove comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def display_url_of(hash_key)
-    # display_link_url(:hash_key => link.hash_key)
     "#{request.scheme}://#{request.host}:#{request.port}/#{hash_key}"
   end
 end
