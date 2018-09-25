227270384f7fc3252eac11a47a9e970ab3b97efc
Nankai Pan <cfanpnk@gmail.com>
2018-09-25 09:46:37 -0500

    Add more system test cases

--- test/system/links_test.rb
+++ test/system/links_test.rb
@@ -7,4 +7,24 @@ class LinksTest < ApplicationSystemTestCase
     click_on "Create Link"
     assert_text "Shortened URL"
   end
+
+  test "expiring a shortened URL" do
+    visit root_path
+    fill_in "Paste a URL to shorten it", with: "roosterteeth.com"
+    click_on "Create Link"
+    find('a.admin').click
+    click_on "Expire"
+    assert_text "The shortened URL has been expired"
+  end
+
+  test "visiting a shortened URL" do
+    visit root_path
+    fill_in "Paste a URL to shorten it", with: "roosterteeth.com"
+    click_on "Create Link"
+    find('a.admin').click
+    find('a.short').click
+    sleep 1
+    page.evaluate_script('window.location.reload()')
+    assert_text "Clicks: 1"
+  end
 end
