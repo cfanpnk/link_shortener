09430c7aab2ed3cbf3b35ff281b5bdeb9ecffa4d
Nankai Pan <cfanpnk@gmail.com>
2018-09-24 23:11:01 -0500

    Use restful style to get shortened URL instead of concatenating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def display_url_of(hash_key)
-    "#{request.scheme}://#{request.host}:#{request.port}/#{hash_key}"
+    shortened_url_url(hash_key)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'links#new'
   resources :links, only: [:create, :show, :index], param: :slug
-  get '/:hash_key' => 'links#forward'
+  get '/:hash_key' => 'links#forward', as: :shortened_url
 
   namespace :admin do
     resources :links, only: [:show, :expire], param: :slug do
