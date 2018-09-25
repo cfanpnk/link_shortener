20136319baa887ae8d3fe238ac577633d75ec675
Nankai Pan <cfanpnk@gmail.com>
2018-09-25 08:31:55 -0500

    Use poltergeist for system testing

--- test/system/links_test.rb
+++ test/system/links_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class LinksTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit links_url
-  #
-  #   assert_selector "h1", text: "Link"
-  # end
+  test "creating a new shortened URL" do
+    visit root_path
+    fill_in "Paste a URL to shorten it", with: "roosterteeth.com"
+    click_on "Create Link"
+    assert_text "Shortened URL"
+  end
 end
